8be0f48227a3967f531d295e0a91270e4bc02e14
andreljunior <andrefsilvajr@gmail.com>
2022-06-28 07:06:44 -0300

    preparacao ambiente

---
+++ alura_django/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\util\20.python\15.django\alura_django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
